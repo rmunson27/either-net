42669b4f97d3b5b62a170aa80d41a4f9294ddf9e
Richard Munson <108591053+rmunson27@users.noreply.github.com>
2022-09-27 19:52:27 -0400

    Moved the many-selectors in the select testing to private static methods (for ease of reuse)

--- Either/EitherTest/SelectTest.cs
+++ Either/EitherTest/SelectTest.cs
@@ -469,19 +469,13 @@ public class SelectTest
     [TestMethod]
     public void TestSelectManyLeft()
     {
-        static Either<ulong, string?> leftF(int i)
-        {
-            if (i % 2 == 0) return i > 0 ? (ulong)i : i.ToString();
-            else return null;
-        }
-
         // Function is called
-        Assert.That.HasLeft(2uL, Either<int, string?>.New(2).SelectManyLeft(leftF));
-        Assert.That.HasRight("-2", Either<int, string?>.New(-2).SelectManyLeft(leftF));
-        Assert.That.HasRight(null, Either<int, string?>.New(3).SelectManyLeft(leftF));
+        Assert.That.HasLeft(2uL, Either<int, string?>.New(2).SelectManyLeft(SingleManySelectorLeft));
+        Assert.That.HasRight("-2", Either<int, string?>.New(-2).SelectManyLeft(SingleManySelectorLeft));
+        Assert.That.HasRight(null, Either<int, string?>.New(3).SelectManyLeft(SingleManySelectorLeft));
 
         // Function is not called
-        Assert.That.HasRight("sss", Either<int, string?>.New("sss").SelectManyLeft(leftF));
+        Assert.That.HasRight("sss", Either<int, string?>.New("sss").SelectManyLeft(SingleManySelectorLeft));
     }
 
     /// <summary>
@@ -492,26 +486,24 @@ public class SelectTest
     [TestMethod]
     public void TestSelectMany()
     {
-        static Either<ulong, float> leftF(int i)
-        {
-            if (i % 2 == 0) return i > 0 ? Either<ulong, float>.NewLeft((ulong)i) : float.NegativeInfinity;
-            else return float.NaN;
-        }
-
-        static Either<ulong, float> rightF(string? s)
-        {
-            if (string.IsNullOrEmpty(s)) return float.PositiveInfinity;
-            else return (ulong)s.Length;
-        }
-
         // Left function is called
-        Assert.That.HasLeft(2uL, Either<int, string?>.New(2).SelectMany(leftF, rightF));
-        Assert.That.HasRight(float.NegativeInfinity, Either<int, string?>.New(-2).SelectMany(leftF, rightF));
-        Assert.That.HasRight(float.NaN, Either<int, string?>.New(3).SelectMany(leftF, rightF));
+        Assert.That.HasLeft(
+            2uL,
+            Either<int, string?>.New(2).SelectMany(BothManySelectorLeft, BothManySelectorRight));
+        Assert.That.HasRight(
+            float.NegativeInfinity,
+            Either<int, string?>.New(-2).SelectMany(BothManySelectorLeft, BothManySelectorRight));
+        Assert.That.HasRight(
+            float.NaN,
+            Either<int, string?>.New(3).SelectMany(BothManySelectorLeft, BothManySelectorRight));
 
         // Right function is called
-        Assert.That.HasRight(float.PositiveInfinity, Either<int, string?>.New("").SelectMany(leftF, rightF));
-        Assert.That.HasLeft(3uL, Either<int, string?>.New("rrr").SelectMany(leftF, rightF));
+        Assert.That.HasRight(
+            float.PositiveInfinity,
+            Either<int, string?>.New(null).SelectMany(BothManySelectorLeft, BothManySelectorRight));
+        Assert.That.HasLeft(
+            3uL,
+            Either<int, string?>.New("rrr").SelectMany(BothManySelectorLeft, BothManySelectorRight));
     }
 
     /// <summary>
@@ -522,19 +514,12 @@ public class SelectTest
     [TestMethod]
     public void TestSelectManyRight()
     {
-        static Either<string?, ulong> rightF(int i)
-        {
-            if (i % 2 == 0) return i > 0 ? (ulong)i : i.ToString();
-            else return null;
-        }
-
         // Function is called
-        Assert.That.HasRight(2uL, Either<string?, int>.New(2).SelectManyRight(rightF));
-        Assert.That.HasLeft("-2", Either<string?, int>.New(-2).SelectManyRight(rightF));
-        Assert.That.HasLeft(null, Either<string?, int>.New(3).SelectManyRight(rightF));
+        Assert.That.HasRight(float.NaN, Either<int, string?>.New(null).SelectManyRight(SingleManySelectorRight));
+        Assert.That.HasLeft(0, Either<int, string?>.New("").SelectManyRight(SingleManySelectorRight));
 
         // Function is not called
-        Assert.That.HasLeft("sss", Either<string?, int>.New("sss").SelectManyRight(rightF));
+        Assert.That.HasLeft(3, Either<int, string?>.New(3).SelectManyRight(SingleManySelectorRight));
     }
     #endregion
     #endregion
@@ -583,6 +568,7 @@ public class SelectTest
     #endregion
 
     #region Selectors
+    #region Select
     /// <summary>
     /// Determines if the <see cref="object"/> passed in is <see langword="null"/>.
     /// </summary>
@@ -652,6 +638,71 @@ public class SelectTest
         await Task.Delay(AsyncCancellableDelay, cancellationToken).ConfigureAwait(false);
         return s.Length;
     }
+    #endregion
+
+    #region SelectMany
+    /// <summary>
+    /// A single-sided many selector for the left side of an <see cref="Either{TLeft, TRight}"/> instance with an
+    /// <see cref="int"/> on the left side or a <see cref="string"/> on the right side.
+    /// </summary>
+    /// <param name="i"></param>
+    /// <returns>
+    /// An <see cref="Either{TLeft, TRight}"/> with the value of (non-negative) <paramref name="i"/> on the left side
+    /// and the string representation of (negative) <paramref name="i"/> on the right side if <paramref name="i"/> is
+    /// even, otherwise <see langword="null"/> on the right side.
+    /// </returns>
+    private static Either<ulong, string?> SingleManySelectorLeft(int i)
+    {
+        if (i % 2 == 0) return i >= 0 ? (ulong)i : i.ToString();
+        else return null;
+    }
+
+    /// <summary>
+    /// A both-sided many selector for the left side of an <see cref="Either{TLeft, TRight}"/> instance with an
+    /// <see cref="int"/> on the left side or a <see cref="string"/> on the right side.
+    /// </summary>
+    /// <param name="i"></param>
+    /// <returns>
+    /// An <see cref="Either{TLeft, TRight}"/> with the (even, non-negative) value of <paramref name="i"/> on the left side
+    /// or <see cref="float.NegativeInfinity"/> on the right side if <paramref name="i"/> is an even negative value,
+    /// otherwise <see cref="float.NaN"/>.
+    /// </returns>
+    private static Either<ulong, float> BothManySelectorLeft(int i)
+    {
+        if (i % 2 == 0) return i >= 0 ? Either<ulong, float>.NewLeft((ulong)i) : float.NegativeInfinity;
+        else return float.NaN;
+    }
+
+    /// <summary>
+    /// A both-sided many selector for the right side of an <see cref="Either{TLeft, TRight}"/> instance with an
+    /// <see cref="int"/> on the left side or a <see cref="string"/> on the right side.
+    /// </summary>
+    /// <param name="i"></param>
+    /// <returns>
+    /// An <see cref="Either{TLeft, TRight}"/> with <see cref="float.PositiveInfinity"/> on the right side if
+    /// <paramref name="s"/> is <see langword="null"/>, otherwise the length of <paramref name="s"/> on the left side.
+    /// </returns>
+    static Either<ulong, float> BothManySelectorRight(string? s)
+    {
+        if (s is null) return float.PositiveInfinity;
+        else return (ulong)s.Length;
+    }
+
+    /// <summary>
+    /// A single-sided many selector for the right side of an <see cref="Either{TLeft, TRight}"/> instance with an
+    /// <see cref="int"/> on the left side or a <see cref="string"/> on the right side.
+    /// </summary>
+    /// <param name="s"></param>
+    /// <returns>
+    /// An <see cref="Either{TLeft, TRight}"/> with <see cref="float.NaN"/> on the right side if <paramref name="s"/>
+    /// is <see langword="null"/>, or the length of <paramref name="s"/> on the left side otherwise.
+    /// </returns>
+    private static Either<int, float> SingleManySelectorRight(string? s)
+    {
+        if (s is null) return float.NaN;
+        else return s.Length;
+    }
+    #endregion
     #endregion
     #endregion
 }
