cb07319d6664232b0b813816ada09d52e7271022
Richard Munson <108591053+rmunson27@users.noreply.github.com>
2022-10-02 13:52:22 -0400

    Shortened the name of helper selectors in select many testing

--- Either/EitherTest/SelectManyTest.cs
+++ Either/EitherTest/SelectManyTest.cs
@@ -22,12 +22,12 @@ public class SelectManyTest
     public void TestSelectManyLeft()
     {
         // Function is called
-        Assert.That.HasLeft(2uL, Either<int, string?>.New(2).SelectManyLeft(SingleManySelectorLeft.Invoke));
-        Assert.That.HasRight("-2", Either<int, string?>.New(-2).SelectManyLeft(SingleManySelectorLeft.Invoke));
-        Assert.That.HasRight(null, Either<int, string?>.New(3).SelectManyLeft(SingleManySelectorLeft.Invoke));
+        Assert.That.HasLeft(2uL, Either<int, string?>.New(2).SelectManyLeft(SingleSelectorLeft.Invoke));
+        Assert.That.HasRight("-2", Either<int, string?>.New(-2).SelectManyLeft(SingleSelectorLeft.Invoke));
+        Assert.That.HasRight(null, Either<int, string?>.New(3).SelectManyLeft(SingleSelectorLeft.Invoke));
 
         // Function is not called
-        Assert.That.HasRight("sss", Either<int, string?>.New("sss").SelectManyLeft(SingleManySelectorLeft.Invoke));
+        Assert.That.HasRight("sss", Either<int, string?>.New("sss").SelectManyLeft(SingleSelectorLeft.Invoke));
     }
 
     /// <summary>
@@ -41,21 +41,21 @@ public class SelectManyTest
         // Left function is called
         Assert.That.HasLeft(
             2uL,
-            Either<int, string?>.New(2).SelectMany(BothManySelectorLeft.Invoke, BothManySelectorRight));
+            Either<int, string?>.New(2).SelectMany(BothSelectorLeft.Invoke, BothSelectorRight));
         Assert.That.HasRight(
             float.NegativeInfinity,
-            Either<int, string?>.New(-2).SelectMany(BothManySelectorLeft.Invoke, BothManySelectorRight));
+            Either<int, string?>.New(-2).SelectMany(BothSelectorLeft.Invoke, BothSelectorRight));
         Assert.That.HasRight(
             float.NaN,
-            Either<int, string?>.New(3).SelectMany(BothManySelectorLeft.Invoke, BothManySelectorRight));
+            Either<int, string?>.New(3).SelectMany(BothSelectorLeft.Invoke, BothSelectorRight));
 
         // Right function is called
         Assert.That.HasRight(
             float.PositiveInfinity,
-            Either<int, string?>.New(null).SelectMany(BothManySelectorLeft.Invoke, BothManySelectorRight));
+            Either<int, string?>.New(null).SelectMany(BothSelectorLeft.Invoke, BothSelectorRight));
         Assert.That.HasLeft(
             3uL,
-            Either<int, string?>.New("rrr").SelectMany(BothManySelectorLeft.Invoke, BothManySelectorRight));
+            Either<int, string?>.New("rrr").SelectMany(BothSelectorLeft.Invoke, BothSelectorRight));
     }
 
     /// <summary>
@@ -68,11 +68,11 @@ public class SelectManyTest
     {
         // Function is called
         Assert.That.HasRight(
-            float.NaN, Either<int, string?>.New(null).SelectManyRight(SingleManySelectorRight.Invoke));
-        Assert.That.HasLeft(0, Either<int, string?>.New("").SelectManyRight(SingleManySelectorRight.Invoke));
+            float.NaN, Either<int, string?>.New(null).SelectManyRight(SingleSelectorRight.Invoke));
+        Assert.That.HasLeft(0, Either<int, string?>.New("").SelectManyRight(SingleSelectorRight.Invoke));
 
         // Function is not called
-        Assert.That.HasLeft(3, Either<int, string?>.New(3).SelectManyRight(SingleManySelectorRight.Invoke));
+        Assert.That.HasLeft(3, Either<int, string?>.New(3).SelectManyRight(SingleSelectorRight.Invoke));
     }
     #endregion
 
@@ -86,7 +86,7 @@ public class SelectManyTest
     /// and the string representation of the (negative) parameter on the right side if the parameter is even,
     /// otherwise <see langword="null"/> on the right side.
     /// </returns>
-    private static readonly FunctionOptions<int, Either<ulong, string?>> SingleManySelectorLeft = new(i =>
+    private static readonly FunctionOptions<int, Either<ulong, string?>> SingleSelectorLeft = new(i =>
     {
         if (i % 2 == 0) return i >= 0 ? (ulong)i : i.ToString();
         else return null;
@@ -101,7 +101,7 @@ public class SelectManyTest
     /// or <see cref="float.NegativeInfinity"/> on the right side if the parameter is an even negative value,
     /// otherwise <see cref="float.NaN"/>.
     /// </returns>
-    private static readonly FunctionOptions<int, Either<ulong, float>> BothManySelectorLeft = new(i =>
+    private static readonly FunctionOptions<int, Either<ulong, float>> BothSelectorLeft = new(i =>
     {
         if (i % 2 == 0) return i >= 0 ? Either<ulong, float>.NewLeft((ulong)i) : float.NegativeInfinity;
         else return float.NaN;
@@ -115,7 +115,7 @@ public class SelectManyTest
     /// An <see cref="Either{TLeft, TRight}"/> with <see cref="float.PositiveInfinity"/> on the right side if the
     /// parameter is <see langword="null"/>, otherwise the length of the parameter on the left side.
     /// </returns>
-    private static readonly FunctionOptions<string?, Either<ulong, float>> BothManySelectorRight = new(s =>
+    private static readonly FunctionOptions<string?, Either<ulong, float>> BothSelectorRight = new(s =>
     {
         if (s is null) return float.PositiveInfinity;
         else return (ulong)s.Length;
@@ -129,7 +129,7 @@ public class SelectManyTest
     /// An <see cref="Either{TLeft, TRight}"/> with <see cref="float.NaN"/> on the right side if the parameter
     /// is <see langword="null"/>, or the length of the parameter on the left side otherwise.
     /// </returns>
-    private static readonly FunctionOptions<string?, Either<int, float>> SingleManySelectorRight = new(s =>
+    private static readonly FunctionOptions<string?, Either<int, float>> SingleSelectorRight = new(s =>
     {
         if (s is null) return float.NaN;
         else return s.Length;
